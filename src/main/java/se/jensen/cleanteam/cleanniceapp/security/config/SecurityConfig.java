7daa6466eded48ab16f7198ba543e058a10790ac
johannajonsson <jojo.risin@outlook.com>
2026-09-15 17:02:00 +0200

    added SecurityConfig

---
+++ src/main/java/se/jensen/cleanteam/cleanniceapp/security/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package se.jensen.cleanteam.cleanniceapp.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableMethodSecurity
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+  // Securityfilterchain
+
+}
